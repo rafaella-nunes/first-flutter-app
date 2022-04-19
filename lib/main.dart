9a4209fd3911c18da98fe6f2a2e92ae37e6018f0
Rafaella Nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-19 10:15:37 -0300

    First flutter app I made, showing a Hello World message

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// import the material file from flutter library
+// ignore_for_file: unused_import
+import 'package:flutter/material.dart';
+
+//define a main function that runs when the application starts
+void main(){
+  // create a new text widget 
+  var app = MaterialApp(
+    home: Text('Hello World!'),
+  );
+
+  //take that widget and place it on the screen
+  runApp(app);
+}
