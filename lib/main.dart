c03d450fc6d98a5d791b78e175afbe50c9cd93f6
Rafaella Nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-26 09:17:52 -0300

    realocating the code for a better practice of clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,13 @@
 // import the material file from flutter library
 import 'package:flutter/material.dart';
 
+//import the app file
+import './src/app.dart';
+
 //define a main function that runs when the application starts
 void main(){
-  //declares the AppBar color
-  const mainColor = Color(0xFFE39EC1);
-  const iconsColor = Color(0xFF77567A);
-  const textColor = Color(0xFF2F323A);
-  // create a new text widget 
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          print('just testing things out!');
-        },
-        backgroundColor: mainColor,
-        child: const Icon(
-          Icons.add,
-          color: iconsColor),
-          ),
-      appBar: AppBar(
-        title: const Text('Cat pics as you like!'),
-        backgroundColor: mainColor,
-        foregroundColor: textColor)
-      ),
-  );
+  runApp(App());
 
   //take the widget and place it on the screen
-  runApp(app);
+
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,33 @@
 //import flutter package
-
 import 'package:flutter/material.dart';
+
+
+//create a class that will be the custom widget, this class extends the StatelessWidget class
+class App extends StatelessWidget{
+   //defines a build method that returns the widget
+  Widget build(context){
+    
+      const mainColor = Color(0xFFE39EC1);
+      const iconsColor = Color(0xFF77567A);
+      const textColor = Color(0xFF2F323A);
+    // create a new text widget 
+    return MaterialApp(
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              print('just testing things out!');
+            },
+            backgroundColor: mainColor,
+            child: const Icon(
+              Icons.add,
+              color: iconsColor),
+              ),
+          appBar: AppBar(
+            title: const Text('Cat pics as you like!'),
+            backgroundColor: mainColor,
+            foregroundColor: textColor)
+        ),
+      );
+  }
+}
+
