cf0a43cb0ed3aa7bc82f43fc97a55cd6c947ee50
Rafaella Nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-26 09:44:17 -0300

    changed the widget to a stateful widget and added a counter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,21 +1,30 @@
 //import flutter package
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget{
+  createState(){
+    return AppState();
+  }
+}
 
-//create a class that will be the custom widget, this class extends the StatelessWidget class
-class App extends StatelessWidget{
+
+//create a class that represents the State of the widget
+class AppState extends State<App>{
+  int counter = 0;
    //defines a build method that returns the widget
   Widget build(context){
-    
       const mainColor = Color(0xFFE39EC1);
       const iconsColor = Color(0xFF77567A);
       const textColor = Color(0xFF2F323A);
     // create a new text widget 
     return MaterialApp(
         home: Scaffold(
+          body: Text('$counter'),
           floatingActionButton: FloatingActionButton(
             onPressed: (){
-              print('just testing things out!');
+              setState(() {
+                counter = counter + 1;
+              });
             },
             backgroundColor: mainColor,
             child: const Icon(
